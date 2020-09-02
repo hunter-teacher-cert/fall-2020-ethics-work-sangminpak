e3638c4c66213df6177285e6de7081a4456e0d5c
sangminpak <67596997+sangminpak@users.noreply.github.com>
2020-09-02 14:19:31 -0400

    Create practice.py

---
+++ wk2wk/00/practice.py
@@ -0,0 +1,19 @@
+print ("Hello World")
+adj1 = input("Enter an adjective")
+print ("You are" + adj1 + "!")
+
+def sentence(n):
+  return print("You are " + n + "!")
+
+print (sentence("bad"))
+
+
+a = int( input ("Enter your age") )
+if a > 17:
+  print ("You can vote")
+elif a <= 17:
+  print ("You're too young to vote")
+
+
+
+//this is all I remember... I didn't learn any arrays, lists, dictionaries, definining function (although I've seen how it's done)
