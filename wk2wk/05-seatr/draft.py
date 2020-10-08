8b9fc9845e2a7564707addcffd8950a228255706
sangminpak <67596997+sangminpak@users.noreply.github.com>
2020-10-08 15:00:17 -0400

    Create draft.py

---
+++ wk2wk/05-seatr/draft.py
@@ -0,0 +1,20 @@
+#My task was to create Passenger class, but I'm still figuring out what/how to do this.
+I have to create Passenger class with instance variables but I feel sooo lost..
+
+
+#this should include basic attributes such as
+#priority number
+#seat number
+#ticket number
+#age
+
+#from Daniel's code:
+
+class Passenger(object):
+    
+    def __init__(self, name, priority):
+        self.name = name
+        self.priority = priority
+
+
+#do I just add stuff within the parenthesis? what do I do to add other variables??
